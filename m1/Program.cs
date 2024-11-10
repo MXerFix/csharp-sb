1223bc769a156238292c843a2703653cc846e144
Maks <maksim2003003@gmail.com>
2024-11-10 23:29:35 +0300

    init m1

---
+++ m1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace m1
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey(true);
+        }
+
+        static void Task2()
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadKey(true);
+        }
+
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+    }
+}
